b06c70be31453746e043182713132b1109f62586
Anders Gjerde <anders.gjerde@protonmail.com>
2025-04-24 14:23:02 +0200

    Indent return to normal

--- engines/sky/sound.cpp
+++ engines/sky/sound.cpp
@@ -1306,7 +1306,7 @@ bool Sound::startSpeech(uint16 textNum) {
 
 	Audio::AudioStream *stream = Audio::makeRawStream(playBuffer, speechSize, rate, Audio::FLAG_UNSIGNED);
 	_mixer->playStream(Audio::Mixer::kSpeechSoundType, &_ingameSpeech, stream, SOUND_SPEECH);
-		return true;
+	return true;
 }
 
 void Sound::fnPauseFx() {
